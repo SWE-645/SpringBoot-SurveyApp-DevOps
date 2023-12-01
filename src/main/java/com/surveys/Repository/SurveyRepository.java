713cda21ef56abd1261a884c5f5384adceb78732
SWE-645 <147286050+SWE-645@users.noreply.github.com>
2023-11-30 22:46:45 -0500

    Delete src directory

--- src/main/java/com/surveys/Repository/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.surveys.Repository;
-
-import com.surveys.Entity.Survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyRepository extends JpaRepository<Survey, Long> {
-    Survey findByEmail(String email);
-
-}
